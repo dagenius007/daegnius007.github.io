7df9f288321d1faf495456ff0c53252ddd54f9a2
Joshua Oluikpe <joshuaoluikpe@gmail.com>
2019-03-27 02:23:50 +0100

    A test app

---
+++ precache-manifest.c8cc1bcc79ff467118bcd26cb36a1496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ae830ad82bb0e1dbdec",
+    "url": "/static/css/main.8e33763c.chunk.css"
+  },
+  {
+    "revision": "7ae830ad82bb0e1dbdec",
+    "url": "/static/js/main.7034caf7.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4f1ee5977c314b730dac",
+    "url": "/static/css/2.10489cc6.chunk.css"
+  },
+  {
+    "revision": "4f1ee5977c314b730dac",
+    "url": "/static/js/2.0e8e4f41.chunk.js"
+  },
+  {
+    "revision": "936061cd39e31678c1fee95deba2b56c",
+    "url": "/index.html"
+  }
+];
